37753335cc173b681ba2e7432fdf6b0e1e388743
Francesco Innocenti <F.Innocenti@sussex.ac.uk>
2024-06-18 13:24:21 +0100

    Test docs build

--- jpc/__init__.py
+++ jpc/__init__.py
@@ -1,21 +1,16 @@
 import importlib.metadata
 
-from .core import (
+from ._core import (
     init_activities_with_ffwd as init_activities_with_ffwd,
     init_activities_from_gaussian as init_activities_from_gaussian,
-    init_activities_with_amort as init_activities_with_amort
-)
-from .core import (
+    init_activities_with_amort as init_activities_with_amort,
     pc_energy_fn as pc_energy_fn,
-    hpc_energy_fn as hpc_energy_fn
-)
-from .core import (
+    hpc_energy_fn as hpc_energy_fn,
     compute_pc_param_grads as compute_pc_param_grads,
     compute_gen_param_grads as compute_gen_param_grads,
-    compute_amort_param_grads as compute_amort_param_grads
+    compute_amort_param_grads as compute_amort_param_grads,
+    solve_pc_activities as solve_pc_activities
 )
-from .core import solve_pc_activities as solve_pc_activities
-
 from ._utils import (
     get_fc_network as get_fc_network,
     compute_accuracy as compute_accuracy
