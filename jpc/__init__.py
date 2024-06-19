8749382e8bbf83de7a40bf934e1ffa3bd4b52eff
Francesco Innocenti <F.Innocenti@sussex.ac.uk>
2024-06-19 12:10:15 +0100

    Add example notebooks

--- jpc/__init__.py
+++ jpc/__init__.py
@@ -6,10 +6,10 @@ from ._core import (
     init_activities_with_amort as init_activities_with_amort,
     pc_energy_fn as pc_energy_fn,
     hpc_energy_fn as hpc_energy_fn,
+    solve_pc_activities as solve_pc_activities,
     compute_pc_param_grads as compute_pc_param_grads,
     compute_gen_param_grads as compute_gen_param_grads,
-    compute_amort_param_grads as compute_amort_param_grads,
-    solve_pc_activities as solve_pc_activities
+    compute_amort_param_grads as compute_amort_param_grads
 )
 from ._utils import (
     get_fc_network as get_fc_network,
@@ -20,6 +20,7 @@ from ._train import (
     make_hpc_step as make_hpc_step
 )
 from ._test import (
+    test_discriminative_pc as test_discriminative_pc,
     test_generative_pc as test_generative_pc,
     test_hpc as test_hpc
 )
--- jpc/_core/__init__.py
+++ jpc/_core/__init__.py
@@ -7,9 +7,9 @@ from ._energies import (
     pc_energy_fn as pc_energy_fn,
     hpc_energy_fn as hpc_energy_fn
 )
+from ._infer import solve_pc_activities as solve_pc_activities
 from ._grads import (
     compute_pc_param_grads as compute_pc_param_grads,
     compute_gen_param_grads as compute_gen_param_grads,
     compute_amort_param_grads as compute_amort_param_grads
 )
-from ._infer import solve_pc_activities as solve_pc_activities
